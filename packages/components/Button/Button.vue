f9524ef6fe6a7aed88dcf489434656c8b5438329
denver-nail <1842852353@qq.com>
2025-01-22 17:47:24 +0800

    继续测试actions

--- packages/components/Button/Button.vue
+++ packages/components/Button/Button.vue
@@ -2,6 +2,7 @@
     <button style="background-color: blue; color:red;">
         this is a button
         测试action是否配置成功
+        继续测试action是否配置成功
     </button>
 </template>
 <script setup lang="ts">
