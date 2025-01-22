5a9e365584ea345b20673390f07ac7553f18e92b
denver-nail <1842852353@qq.com>
2025-01-22 16:38:17 +0800

    续：导入样式文件

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,6 +1,7 @@
 import { makeInstaller } from "@hangui/utils";
 import components from "./components";
-// import "@hangui/theme/index.css";
+//导入样式文件
+import "@hangui/theme/index.css";
 //通过返回的 installer，可以将整个插件集合作为一个插件进行统一安装。
 const installer = makeInstaller(components);
 //core作为所有组件暴露的出口
